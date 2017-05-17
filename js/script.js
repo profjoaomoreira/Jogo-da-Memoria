40f40b1c7b0af386210c3bc6cd2a0ae4ff159bc3
Laércio <laerciogoncalvesdonascimento@gmail.com>
2017-05-17 02:30:09 -0300

    Adicionando efeito de transição para todas as cartas e mostrando imagens diferentes com Javascript

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 (function() {
+    var images = [];
+
+    for (var i = 0; i < 16; i++) {
+        var img = {
+            src: "img/" + i + ".jpg",
+            id: i % 8
+        };
+        images.push(img);
+    }
+
     startGame();
 
     function startGame() {
+        var frontFaces = document.getElementsByClassName("front");
+
         for (var i = 0; i < 16; i++) {
-            //var card = document.getElementById("card" + i);
-            var card = document.querySelector("#card" + i);
+            var card = document.getElementById("card" + i);
+            //var card = document.querySelector("#card" + i);
             card.style.left = i % 8 === 0 ? 5 + "px" : i % 8 * 165 + 5 + "px";
             card.style.top = i < 8 ? 5 + "px" : 250 + "px";
 
             card.addEventListener("click", flipCard, false);
+
+            frontFaces[i].style.background = "url('" + images[i].src + "')";
+            frontFaces[i].setAttribute("id", images[i].id);
         }
 
         function flipCard() {
